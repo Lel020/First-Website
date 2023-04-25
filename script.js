2e78fa9272aed7bc74e01cc3a111992cf97cc7de
lel020 <mahmud.jawaad03@gmail.com>
2023-04-25 19:45:44 +1000

    Reformatted and removed dead code

--- script.js
+++ script.js
@@ -1,22 +1,20 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const discordButton = document.getElementById('discord-button');
-    discordButton.addEventListener('click', () => {
-      try {
-        window.open('https://discord.com/users/bobaguard%1001');
-      } catch(e) {
-        window.location.href = 'https://discord.com';
-        console.log(e);
-      }
-    });
+const discordButton = document.getElementById('discord-button');
+discordButton.addEventListener('click', () => {
+  try {
+    window.open('https://discord.com/users/bobaguard%1001');
+  } catch(e) {
+    window.location.href = 'https://discord.com';
+    console.log(e);
+  }
+});
 
-    const githubbutton = document.getElementById('github-button');
-    githubbutton.addEventListener('click', () => {
-      try {
-          window.open('https://github.com/Lel020');
-          }catch (e) {
-          window.location.href = 'https://github.com/';
-          console.log(e);
-      }
-  });
+const githubbutton = document.getElementById('github-button');
+githubbutton.addEventListener('click', () => {
+  try {
+    window.open('https://github.com/Lel020');
+  }catch (e) {
+    window.location.href = 'https://github.com/';
+    console.log(e);
+ }
 });
   
